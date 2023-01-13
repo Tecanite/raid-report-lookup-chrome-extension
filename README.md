dfb3154b40fe1c50e11e46cedb233ec4effd6d4b
Jannik Rönisch <jannik.roenisch@tu-ilmenau.de>
2023-01-13 23:41:59 +0100

    Version 0.1
    First Version:
    Bugs: Raid Report error (invalid Parameters) because of non matching Platforms

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # raid-report-lookup-chrome-extension
-Chrome extension, that adds options to quickly track a player's raid/dungeon report to the context menu
+
+Chrome extension, that adds options to quickly track a player's raid/dungeon report to the context menu.
---
+++ background.js
@@ -0,0 +1,37 @@
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.contextMenus.create({
+        "title": 'Raid Report (Link)',
+        "contexts": ["link"],
+        "id": "raid_link",
+        "documentUrlPatterns": ["*://*.bungie.net/en/ClanV2/PublicFireteam?*"]
+    });
+    chrome.contextMenus.create({
+        "title": 'Dungeon Report (Link)',
+        "contexts": ["link"],
+        "id": "dungeon_link",
+        "documentUrlPatterns": ["*://*.bungie.net/en/ClanV2/PublicFireteam?*"]
+    });
+});
+
+
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    if(tab) {
+        var str = info.linkUrl,
+            rmv = "https://www.bungie.net/7/en/User/Profile/";
+        str = str.replace(rmv, '');
+        var char1 = str.charAt(0);
+        str = str.replace(char1 +'/', '');
+        if (info.menuItemId === "dungeon_link"){
+            chrome.tabs.create({  
+                url: "https://dungeon.report/pc/" + encodeURIComponent(str)
+            });
+        }
+        if (info.menuItemId === "raid_link") {
+
+            chrome.tabs.create({  
+                url: "https://raid.report/pc/" + encodeURIComponent(str)
+            });
+        }
+    } 
+});
